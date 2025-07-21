2131b22801459535b08e0584ba2b9f4982bcf756
DevJin27 <devjindal2317@gmail.com>
2025-07-21 11:50:30 +0530

    fixed supabase login

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://mhzzqnwqfrysmmhwcwdq.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oenpxbndxZnJ5c21taHdjd2RxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzMzk0NDcsImV4cCI6MjA2MTkxNTQ0N30.O-OdOrR-1TlDQWEQWzX4f9uIT__Lvh23RGHtZ843_kM"
+const supabaseUrl = "https://ndmodtrutsknrijzsrvt.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kbW9kdHJ1dHNrbnJpanpzcnZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwMTUyNDEsImV4cCI6MjA2ODU5MTI0MX0.i3mXTW9_y2T0BBWaKj2_g6AjexZ4Qd8uyNIysgA05L4"
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
